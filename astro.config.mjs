e17dced78e546fd0c5699b303dbfa5a427e2658f
puria <puria@dyne.org>
2025-01-25 18:17:26 +0100

    feat: sitemap + social cover/descriptions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://giallogialloproject.com',
+  integrations: [sitemap()],
   vite: { plugins: [tailwindcss()] },
 });
--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,23 +1,5 @@
-import { createDirectus, rest, type DirectusField } from "@directus/sdk";
-
-type Global = {
-  id: number;
-  title: string;
-  about: DirectusField;
-};
-
-type Exhibitions = {
-  title: string;
-  slug: string;
-  content: string;
-}
-
-interface Schema {
-  global: Global;
-  exhibitions: Exhibitions;
-}
+import { createDirectus, rest } from "@directus/sdk";
 
 const directusUrl = process.env.DIRECTUS_URL || "http://127.0.0.1:8055";
-const directus = createDirectus<Schema>(directusUrl).with(rest());
-
+const directus = createDirectus(directusUrl).with(rest());
 export default directus;
