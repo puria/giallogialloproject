2cc69e94c874d7f902eb43a9a2136fb3d992d06f
puria <puria@dyne.org>
2025-01-25 02:35:57 +0100

    feat: read from env the admin url

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -17,6 +17,7 @@ interface Schema {
   exhibitions: Exhibitions;
 }
 
-const directus = createDirectus<Schema>("http://127.0.0.1:8055").with(rest());
+const directusUrl = process.env.DIRECTUS_URL || "http://127.0.0.1:8055";
+const directus = createDirectus<Schema>(directusUrl).with(rest());
 
 export default directus;
