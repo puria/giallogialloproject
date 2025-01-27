e2799b74c3f9c0f2cfd1687624958f786152041c
puria <puria@dyne.org>
2025-01-27 23:54:05 +0100

    fix: hide transforms sizes

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,5 +1,5 @@
 import { createDirectus, rest } from "@directus/sdk";
 
-const directusUrl = process.env.DIRECTUS_URL || "http://127.0.0.1:8055";
+const directusUrl = import.meta.env.DIRECTUS_URL || "http://127.0.0.1:8055";
 const directus = createDirectus(directusUrl).with(rest());
 export default directus;
